fbe5d6c523fff5513e1bd36d16e97f13c392eddf
Yana_Aziamblouskaya <yanaozembl@gmail.com>
2023-12-05 14:51:53 +0100

    refactor pom.xml

--- src/main/java/org/example/daos/UserDAO.java
+++ src/main/java/org/example/daos/UserDAO.java
@@ -17,26 +17,26 @@ public class UserDAO {
     public UserDAO(JdbcTemplate jdbcTemplate) {this.jdbcTemplate = jdbcTemplate; }
 
     public List<User> getAll() {
-        return jdbcTemplate.query("SELECT * FROM Users", new BeanPropertyRowMapper<>(User.class));
+        return jdbcTemplate.query("SELECT * FROM users", new BeanPropertyRowMapper<>(User.class));
     }
 
     public User get(int id) {
-        return jdbcTemplate.query("SELECT * FROM Users WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(User.class))
+        return jdbcTemplate.query("SELECT * FROM users WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(User.class))
                 .stream().findAny().orElse(null);
     }
 
     public void save(User user){
-        jdbcTemplate.update(("INSERT INTO Users(name, email) VALUES ( ?, ?)"),
+        jdbcTemplate.update(("INSERT INTO users(name, email) VALUES ( ?, ?)"),
             user.getName(), user.getEmail());
     }
 
     public void update(int id, User updatedUser){
-        jdbcTemplate.update(("UPDATE Users SET name=?, email=? WHERE id=?"),
+        jdbcTemplate.update(("UPDATE users SET name=?, email=? WHERE id=?"),
                 updatedUser.getName(), updatedUser.getEmail(), id);
     }
 
     public void delete(int id) {
-        jdbcTemplate.update(("DELETE FROM Users WHERE id=?"), id);
+        jdbcTemplate.update(("DELETE FROM users WHERE id=?"), id);
     }
 
 }
